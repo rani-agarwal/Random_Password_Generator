6ccefa558aa39fc8f998f190b9f1f900ea6eab56
Rani Agarwal <raniag681@gmail.com>
2021-06-12 16:37:41 +0530

    changes

--- index.php
+++ index.php
@@ -1,18 +1,24 @@
 <?php
+function generatePassword() 
+{
+        $len=$_POST['password']??10;
+        
+        $small = 'abcdefghijklmnopqrstuvwxyz';            
+        $caps  = strtoupper($small);               
+        $numerics   = '1234567890';                  
+        $specialChars = '!@#$%^&*_';   
 
-if(isset($_POST['password'])){
-$char ="0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ!$^%&*@#_";
-$pwd=substr(str_shuffle($char),0,$_POST['password']);
-}
-else{
-    $char ="0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ!$^%&*@#_";
-    $pwd=substr(str_shuffle($char),0,0);
-    
-}
+    $allchar = $small.$caps.$numerics.$specialChars; 
+    $password = '';         
 
+    for($i = 0; $i < $len; $i++) {                                 
+        $rand = rand(0, strlen($allchar) - 1);   //The rand() function generates a random integer and The strlen() function returns the length of a string.               
+        $password .= substr($allchar, $rand, 1); //The strlen() function returns the length of a string.substr(string,start,length)               
+    }
+    return $password;       
+}
 ?>
-
-<h2 align=center>Random Password Generator="<?php echo $pwd; ?>"</h2>
+<h2 align=center>Random Password Generator="<?php echo generatePassword(); ?>"</h2>
 <h3 align=center>"0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ!$^%&*@#_"</h3>
 
 <form method="POST" action="" enctype="multipart/form-data">
@@ -23,4 +29,3 @@ else{
 <button type="submit">Submit</button>
 <br><br>
 </form>
-
